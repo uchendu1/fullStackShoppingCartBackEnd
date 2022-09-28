adac340ba5fbf5fa7606fcfe9424a13fd60b37aa
linda <lindatochukwu30@gmail.com>
2022-09-28 12:59:05 +0100

    authentication

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,66 @@
 const router = require("express").Router();
+const CryptoJs = require("crypto-js"); // auth for register
+const User = require("../models/User");
 
-router.get("/userTest", (req, res) =>{
-    res.send("user test is successful!!!");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //check if the user changed its password:
+  if (req.body.password) {
+    req.body.password = CryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
-
-router.post("/userTest", (req, res) =>{
-    const userName = req.body.userName;
-    res.send("user test post is successful!!!");
+//delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
+//get  one user
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get all users
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new
+  try {
+    const users = query ?  await User.find().sort({_id: -1}).limits(5) : User.find(1);
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
