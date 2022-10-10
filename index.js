fb51a82af1d84f3c095d49ec86884fc5223b039f
linda <lindatochukwu30@gmail.com>
2022-10-10 10:48:09 +0100

    stripe.js modified

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+
 
 
 
@@ -23,12 +26,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+  app.use(cors());
 app.use(express.json());
   app.use("/api/auth", authRoute)
   app.use("/api/users", userRoute)
   app.use("/api/products", productRoute)
   app.use("/api/cart", cartRoute)
   app.use("/api/orders", orderRoute)
+  app.use("/api/checkout", stripeRoute)
+
 
 
   
